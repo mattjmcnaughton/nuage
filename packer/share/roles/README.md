5dba513d9d90e62adedfbaa52b55b4bac3738dc2
mattjmcnaughton <mattjmcnaughton@gmail.com>
2019-12-22 09:41:22 -0500

    Add firewall role
    We add a `firewall` role, which configures our hosts to accept/deny
    requests on certain ports. We will additionally use the cloud firewall
    of whatever cloud provider we use (i.e. AWS Security Groups), but it
    never hurts to have the additional protection. I made in effort to write
    the firewall role in an os agnostic manner. Maybe I will one day deploy
    BSD hosts in prod... it would be nice to have a consistent interface.
    
    Attempting to add the `firewall` role exposed an issue with installing
    `unattended-upgrade` in our base image. Whenever the base image boots,
    so that we can apply additional provisioning to create our application
    images, unattended-upgrades immediately runs and hogs the apt lock for
    minutes. Ultimately, we decided uninstalling unattened-upgrades in the
    base image, and reinstalling/configuring it in the application image, is
    the easiest way to address this issue.
    
    Finally, we add `ansible-lint` back, as we are comfortable installing
    the dependency.
    
    The next diff will be adding an nginx role.

---
+++ packer/share/roles/README.md
@@ -0,0 +1,9 @@
+# mattjmcnaughton.firewall
+
+Notes to expand on:
+- Use _in addition_ to security groups. Additional layer of protection.
+
+Variables:
+NOTE: Eventually (?) should we prefix all variables with their role name?
+- `ufw_incoming_ports_to_allow`: List of incoming ports to allow.
+- `ufw_incoming_ports_to_deny`: List of incoming ports to deny.
---
+++ packer/share/roles/mattjmcnaughton.firewall/test/integration/default/spec.rb
@@ -0,0 +1,21 @@
+if os.family == 'debian'
+  describe package("ufw") do
+    it { should be_installed }
+  end
+
+  describe port(22) do
+    it { should be_listening }
+  end
+
+  # We don't really have a great way to use inspec to test that the expected
+  # ports are NOT listening.
+
+  # Our `os-hardening` role has opinions on these `sysctl` values. Essentially
+  # we are ensuring the firewall changes we make doesn't mess them up.
+  describe kernel_parameter('net.ipv4.conf.all.log_martians') do
+    its(:value) { should eq 1 }
+  end
+  describe kernel_parameter('net.ipv4.conf.default.log_martians') do
+    its(:value) { should eq 1 }
+  end
+end
