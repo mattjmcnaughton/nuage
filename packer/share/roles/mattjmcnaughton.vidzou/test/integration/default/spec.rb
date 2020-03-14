c30771d0739500e5fdddb19b8faf4d8b7e3b88da
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-03-13 22:53:59 -0400

    Finish vidzou ansible role
    Finish the ansible role for deploying vidzou. Nginx is upstreaming
    requests to the Go server and we're reading environment variables from a
    `.env` file, so we should be good to go.

--- packer/share/roles/mattjmcnaughton.vidzou/test/integration/default/spec.rb
+++ packer/share/roles/mattjmcnaughton.vidzou/test/integration/default/spec.rb
@@ -6,6 +6,24 @@ describe docker_container('vidzou') do
   its('ports') { should eq "0.0.0.0:8080->8080/tcp" }
 end
 
-# Test nginx running
-#
-# Test nginx port-forwarding to container
+describe service('nginx') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe http('http://localhost/', ssl_verify: false) do
+  its('status') { should cmp 301 }
+end
+
+describe http('https://localhost/', ssl_verify: false) do
+  its('status') { should cmp 401 }
+end
+
+describe http('https://localhost/',
+              auth: { user: 'matt', pass: 'insecure' },
+              headers: { 'Accept-Encoding' => 'gzip,deflate'},
+              ssl_verify: false) do
+  its('status') { should cmp 200 }
+  its('body') { should include 'vidzou' }
+end
