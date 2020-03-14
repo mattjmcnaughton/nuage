8ebde6263b378400275f02f95d392e4ada498ee4
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-03-13 22:21:56 -0400

    Initial kitchen tests for vidzou passing
    The initial vidzou tests are passing (i.e. installing all dependencies
    and running container). The final step (for the vidzou playbook) is
    the `env_file` and wiring nginx to talk to the docker container.

--- packer/share/roles/mattjmcnaughton.vidzou/test/integration/default/spec.rb
+++ packer/share/roles/mattjmcnaughton.vidzou/test/integration/default/spec.rb
@@ -1,4 +1,11 @@
-describe docker.containers.where { names == 'vidzou' } do
+describe docker_container('vidzou') do
+  it { should exist }
   it { should be_running }
-  its('images') { should include 'vidzou' }
+  its('id') { should_not eq '' }
+  its('image') { should include 'vidzou' }
+  its('ports') { should eq "0.0.0.0:8080->8080/tcp" }
 end
+
+# Test nginx running
+#
+# Test nginx port-forwarding to container
