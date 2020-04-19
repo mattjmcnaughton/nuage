ea482b4bca52f584d69b5f3b7b271337ab583ed4
mattjmcnaughton <mattjmcnaughton@gmail.com>
2020-04-18 23:25:31 -0400

    Add s3 access logging for vidzou
    Add access logs for the vidzou bucket, so I can see access patterns.

--- terraform/modules/vidzou/s3.tf
+++ terraform/modules/vidzou/s3.tf
@@ -1,6 +1,24 @@
+resource "aws_s3_bucket" "vidzou_log_bucket" {
+  bucket = "${local.vidzou_bucket}-logs"
+  acl = "log-delivery-write"
+
+  tags = {
+    Name = "${local.vidzou_bucket}-logs"
+    name = "${local.vidzou_bucket}-logs"
+    environment = var.environment
+    Terraform = "true"
+    project = "nuage"
+  }
+}
+
 resource "aws_s3_bucket" "vidzou_bucket" {
   bucket = local.vidzou_bucket
   acl = "private"
+
+  logging {
+    target_bucket = aws_s3_bucket.vidzou_log_bucket.id
+    target_prefix = "log/"
+  }
 
   tags = {
     Name = local.vidzou_bucket
