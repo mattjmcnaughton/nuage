290aae3004c005002f74607efd8d07d635bdaf2c
mattjmcnaughton <mattjmcnaughton@gmail.com>
2021-01-18 10:33:34 -0500

    Remove vidzou
    I'm not really using vidzou at all these days. If I decide to later,
    should be simple to relaunch.

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -57,26 +57,3 @@ module "blog" {
     module.base.aws_lb_target_group_default_arn
   ]
 }
-
-module "vidzou" {
-  source = "../../modules/vidzou"
-
-  environment = local.environment
-
-  vpc_id = module.vpc.vpc_id
-  public_subnet_ids = module.vpc.public_subnets
-
-  extra_host_iam_policy = [
-    module.base.aws_iam_policy_access_mattjmcnaughton_ssl_certs_arn
-  ]
-
-  extra_host_security_groups = [
-    module.base.aws_security_group_allow_ssh_ingress_from_all_id,
-    module.base.aws_security_group_allow_http_ingress_from_main_alb_id,
-    module.base.aws_security_group_allow_https_ingress_from_main_alb_id
-  ]
-
-  main_aws_lb_dns_name = module.base.aws_lb_main_dns_name
-  main_aws_lb_zone_id = module.base.aws_lb_main_zone_id
-  main_aws_lb_listener_https_arn = module.base.aws_lb_listener_main_https_arn
-}
