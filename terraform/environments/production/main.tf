922c493c280556d822548dcb62ed58ea9f97545b
mattjmcnaughton <me@mattjmcnaughton.com>
2024-04-06 19:16:40 -0400

    chore: run `terraform fmt`

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -4,38 +4,38 @@ locals {
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = "${local.name_prefix}-nuage-vpc"
   cidr = "10.0.0.0/16"
 
-  azs = ["us-east-1a", "us-east-1b"]
+  azs            = ["us-east-1a", "us-east-1b"]
   public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
   public_subnet_tags = {
     public = "true"
   }
 
   tags = {
-    Name = "${local.name_prefix}-nuage-vpc"
-    name = "${local.name_prefix}-nuage-vpc"
+    Name        = "${local.name_prefix}-nuage-vpc"
+    name        = "${local.name_prefix}-nuage-vpc"
     environment = local.environment
-    Terraform = "true"
-    project = "nuage"
+    Terraform   = "true"
+    project     = "nuage"
   }
 
   # Changes when upgrading the `vpc` module.
-  manage_default_network_acl = false
-  manage_default_route_table = false
+  manage_default_network_acl    = false
+  manage_default_route_table    = false
   manage_default_security_group = false
-  enable_dns_hostnames = false
-  map_public_ip_on_launch = true
+  enable_dns_hostnames          = false
+  map_public_ip_on_launch       = true
 }
 
 module "base" {
   source = "../../modules/base"
 
   environment = local.environment
-  vpc_id = module.vpc.vpc_id
+  vpc_id      = module.vpc.vpc_id
 }
 
 module "blog" {
@@ -43,7 +43,7 @@ module "blog" {
 
   environment = local.environment
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id            = module.vpc.vpc_id
   public_subnet_ids = module.vpc.public_subnets
 
   extra_host_iam_policy = [
--- terraform/environments/production/provider.tf
+++ terraform/environments/production/provider.tf
@@ -4,9 +4,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "p-mattjmcnaughton-tf-storage"
-    key = "nuage"
-    region = "us-east-1"
+    bucket  = "p-mattjmcnaughton-tf-storage"
+    key     = "nuage"
+    region  = "us-east-1"
     encrypt = true
   }
 }
