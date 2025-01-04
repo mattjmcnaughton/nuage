fc1ed909586a24582712409f03a7122c2aff3f54
mattjmcnaughton <me@mattjmcnaughton.com>
2025-01-04 15:10:11 -0500

    chore: delete mattjmcnaughton-io hosted zone
    I don't own mattjmcnaughton.io, so saving the ~$.50 per month for each
    hosted zone.

--- terraform/environments/global/route53.tf
+++ terraform/environments/global/route53.tf
@@ -1,6 +1,5 @@
 # We only have the one top level zone for `mattjmcnaughton.com`.
-# It is our public facing zone. All `dev`, `staging`, `prod`, etc...
-# delinieations should occur in the private `.io` zone.
+# It is our public facing zone.
 #
 # If the top level zones already exist (i.e. created by AWS when we purchased
 # the domain), we can import them using `terraform import
@@ -9,95 +8,12 @@ resource "aws_route53_zone" "mattjmcnaughton_com" {
   name = "mattjmcnaughton.com"
 }
 
-resource "aws_route53_zone" "mattjmcnaughton_io" {
-  name = "mattjmcnaughton.io"
-}
-
 resource "aws_route53_zone" "worldsbestpug_com" {
   name = "worldsbestpug.com"
 }
 
 resource "aws_route53_zone" "thatsagoodpug_com" {
   name = "thatsagoodpug.com"
-}
-
-# @TODO(mattjmcnaughton) We could extract the `aws_route53_zone` and
-# `aws_route53_record` necessary for creating a subdomain into a tested module.
-#
-# @TODO(mattjmcnaughton) It is also a fair question whether defining these
-# route53 zones/nameservice records should live in the `global` terraform
-# directory or in the respective environment directories. I'm going to leave it
-# in global for now... but it may make sense to change it later.
-resource "aws_route53_zone" "dev_mattjmcnaughton_io" {
-  name = "dev.mattjmcnaughton.io"
-}
-
-resource "aws_route53_record" "dev_mattjmcnaughton_io_ns" {
-  zone_id = aws_route53_zone.mattjmcnaughton_io.zone_id
-  name = "dev.mattjmcnaughton.io"
-  type = "NS"
-  ttl = "30"
-
-  records = [
-    aws_route53_zone.dev_mattjmcnaughton_io.name_servers.0,
-    aws_route53_zone.dev_mattjmcnaughton_io.name_servers.1,
-    aws_route53_zone.dev_mattjmcnaughton_io.name_servers.2,
-    aws_route53_zone.dev_mattjmcnaughton_io.name_servers.3,
-  ]
-}
-
-resource "aws_route53_zone" "staging_mattjmcnaughton_io" {
-  name = "staging.mattjmcnaughton.io"
-}
-
-resource "aws_route53_record" "staging_mattjmcnaughton_io_ns" {
-  zone_id = aws_route53_zone.mattjmcnaughton_io.zone_id
-  name = "staging.mattjmcnaughton.io"
-  type = "NS"
-  ttl = "30"
-
-  records = [
-    aws_route53_zone.staging_mattjmcnaughton_io.name_servers.0,
-    aws_route53_zone.staging_mattjmcnaughton_io.name_servers.1,
-    aws_route53_zone.staging_mattjmcnaughton_io.name_servers.2,
-    aws_route53_zone.staging_mattjmcnaughton_io.name_servers.3,
-  ]
-}
-
-resource "aws_route53_zone" "prod_mattjmcnaughton_io" {
-  name = "prod.mattjmcnaughton.io"
-}
-
-resource "aws_route53_record" "prod_mattjmcnaughton_io_ns" {
-  zone_id = aws_route53_zone.mattjmcnaughton_io.zone_id
-  name = "prod.mattjmcnaughton.io"
-  type = "NS"
-  ttl = "30"
-
-  records = [
-    aws_route53_zone.prod_mattjmcnaughton_io.name_servers.0,
-    aws_route53_zone.prod_mattjmcnaughton_io.name_servers.1,
-    aws_route53_zone.prod_mattjmcnaughton_io.name_servers.2,
-    aws_route53_zone.prod_mattjmcnaughton_io.name_servers.3,
-  ]
-}
-
-resource "aws_route53_zone" "global_mattjmcnaughton_io" {
-  name = "global.mattjmcnaughton.io"
-}
-
-resource "aws_route53_record" "global_mattjmcnaughton_io_ns" {
-  zone_id = aws_route53_zone.mattjmcnaughton_io.zone_id
-  name = "global.mattjmcnaughton.io"
-  type = "NS"
-  ttl = "30"
-
-  records = [
-    aws_route53_zone.global_mattjmcnaughton_io.name_servers.0,
-    aws_route53_zone.global_mattjmcnaughton_io.name_servers.1,
-    aws_route53_zone.global_mattjmcnaughton_io.name_servers.2,
-    aws_route53_zone.global_mattjmcnaughton_io.name_servers.3,
-  ]
 }
 
 # Configuration for fastmail to send email via mattjmcnaughton.com.
